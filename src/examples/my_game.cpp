6cc7bc6c72b71d69ff5bcb03f116929748c4fee0
adamsocki <adamsocki@ADAMSOCKI0964>
2022-08-31 19:30:27 -0500

    functional entity manager. need to clean up

--- src/examples/my_game.cpp
+++ src/examples/my_game.cpp
@@ -1,63 +1,3 @@
-
-//enum EntityType {
-//	EntityType_Player,
-//	EntityType_Enemy,
-//	EntityType_Count
-//};
-//
-//struct EntityInfo {
-//	EntityType type;
-//	int32 generation;
-//	int32 indexInBuffer;
-//};
-//
-//struct EntityHandle {
-//	int32 id;
-//	int16 generation;
-//	EntityType type;
-//};
-//
-//struct EntityManager {
-//	int32 count;
-//	int32 capacity;
-//	EntityInfo* entities;
-//	int32 nextFreeID;
-//};
-//
-//struct EntityTypeBuffer {
-//    int32 count;
-//    int32 entitySize;
-//    int32 capacity;
-//    void *entities;
-//	
-//	//EntityTypeBuffer buffers[EntityType_Count];
-//};
-//
-//
-//EntityHandle AddEntity(EntityManager *entityManager, EntityType type) {
-//	
-//	EntityHandle entityHandle = {};
-//
-//	EntityInfo *entityInfo = entityManager->entities[entityManager->nextFreeID];
-//	
-//	
-//	/*info->type = type;
-//	
-//	EntityTypeBuffer *buffer = &entityManager->buffers[type];
-//	info->indexInBuffer = buffer->count;
-//	
-//	buffer->count++;
-//	
-//	entityHandle.id = entityManager->nextFreeID;
-//	entityHandle.generation = info->generation;
-//	
-//	entityManager->nextID++;*/
-//	
-//	return entityHandle;
-//}
-//
-//EntityManager *em = NULL;
-
 enum EntityType {
 	EntityType_Player,
 	EntityType_Count   
@@ -71,15 +11,52 @@ struct EntityTypeBuffer {
 };
 
 struct EntityManager {
-	EntityTypeBuffer buffers[EntityType_Count];
+	EntityTypeBuffer buffers[EntityType_Player];
 };
 
 struct MyData {
 	Sprite sprite;
 };
 
+struct Player {
+	int32 index;
+	//vec2 size;
+	vec2 position;
+	int32 hitPoints;
+	Sprite* sprite;
+};
+
 MyData* Data = NULL;
 EntityManager* em = NULL;
+
+Player p;
+//int32 playerCount = 1;
+
+int32 AddEntity(EntityType type, EntityManager* data) {
+
+	
+	// Look up the buffer in type
+	EntityTypeBuffer* buffer = &data->buffers[type];
+	// count tells us where in the buffer we should add our data
+	int32 currentCount = buffer->count;
+	// copy data into buffer
+	buffer->entities = data;
+	// increment count
+	int32 index;
+	index = buffer->count++;
+	
+	// return index to our data in the buffer
+	return index;
+}
+
+
+void GetEntity(EntityType type, int32 index) {
+	
+	EntityTypeBuffer* buffer = &em->buffers[type];
+	// look up the buffer using type then use the index into data 
+	buffer[index];
+}
+
 
 void MyInit() {
 	Game->myData = malloc(sizeof(MyData));
@@ -87,15 +64,47 @@ void MyInit() {
 
 	Data = (MyData*)Game->myData;
 	LoadSprite(&Data->sprite, "data/player_guy.png");
+
+	em = (EntityManager*)Game->myData;
 	
+	em->buffers[EntityType_Player].entities = (Player *)malloc(sizeof(Player) * 1);
+	memset(em->buffers, 0, sizeof(Player));
+
+	
+	p.index = AddEntity(EntityType_Player, em);
+	p.index = AddEntity(EntityType_Player, em);
+	//em->buffers[EntityType_Player].
+	Player* p2 = (Player*)em->buffers[0].entities;
+	p2->position.x = 3;
+	//p2->size.x = 0.3f;
+	//p2->size.y = 0.3f;
 }
 
 void MyGameUpdate() {
 	// LOGIC
+	Player* p3 = (Player*)em->buffers[0].entities;
+
+	
+	if (InputPressed(Input, Input_Up)) {
+		p3->position.y++;
+	}
+	if (InputPressed(Input, Input_Down)) {
+		p3->position.y--;
+	}
+	if (InputPressed(Input, Input_Right)) {
+		p3->position.x++;
+	}
+	if (InputPressed(Input, Input_Left)) {
+		p3->position.x--;
+	}
+	
 	
 	// RENDER
+	//GetEntity(EntityType_Player, p.index);
+	
+	
 	ClearColor(RGB(0.4f, 0.0f, 0.0f));
-	DrawSprite(V2(0), V2(0.3f,0.3f), &Data->sprite);
+	DrawSprite(p3->position, V2(0.3f, 0.3f), &Data->sprite);
 }
 
 
